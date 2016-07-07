562fe9254ec90cd76bb28b73428bf6da4f6c3969
Peter Qiu <peterqiu1997@gmail.com>
2016-07-07 13:25:24 -0700

    changed stats box

--- src/js/Statistics.js
+++ src/js/Statistics.js
@@ -9,10 +9,10 @@ export default class Statistics extends React.Component {
 	render() { 
 		return ( // Text is rendered as span in React 
 			<div class = "bottom stats">
+				<div class = "timeframe">{"Past Day"}</div>
 				<ul>
-					<li>Mean: {"5500"}</li>
-					<li>Deviation: {"0.27"}</li>
-					<li>Trend: Upwards</li>
+					<li>μ: {"5500"}</li>
+					<li>σ: {"0.27"}</li>
 				</ul>
 			</div>
 		);
