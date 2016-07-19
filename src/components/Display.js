d98b6ca569d8afea3f988fe8614fb4d6e20f4bae
Peter Qiu <peterqiu1997@gmail.com>
2016-07-19 09:52:17 -0700

    compression caused app crash

--- src/components/Display.js
+++ src/components/Display.js
@@ -2,8 +2,8 @@ import React from "react";
 
 export default class Display extends React.Component {
 
-    constructor() {
-        super();
+    constructor(props) {
+        super(props);
     }
 
     render() {
--- src/components/Statistics.js
+++ src/components/Statistics.js
@@ -1,8 +1,8 @@
 import React from "react";
 
 export default class Statistics extends React.Component {
-	constructor() {
-		super();
+	constructor(props) {
+		super(props);
 	}
 
 	render() { 
