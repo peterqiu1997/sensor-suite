57cc986606a5e78e6a68dd6e8a3e87196e83886c
Peter Qiu <peterqiu1997@gmail.com>
2016-07-18 17:19:22 -0700

    connected to mongoose

--- server.js
+++ server.js
@@ -3,19 +3,24 @@
 const express =  require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const compression = require('compression');
 const DataModel = require('./DataModel');
+
 const port = process.env.PORT || 3000;
 const uristring = process.env.MONGODB_URI || "***REMOVED***"; 
-// CHANGE THE ABOVE TO VARIABLE LATER
+
 const app = express();
 const db = mongoose.connection;
+const duration = 1000;
+let connected = 0,
+    open = false;
 
 const server = app.listen(port, function() {
     console.log('listening on port ' + port);
 });
 
+app.use(compression());
 app.use(express.static(path.join(__dirname, '/dist')));
-
 app.get('/', function(req, res) {
     res.sendFile(path.resolve(__dirname + '/dist/index.html'));
 });
@@ -28,30 +33,9 @@ mongoose.connect(uristring, function(err, res) {
       }
 });
 
-const io = require('socket.io')(server);
-
-const pulse = setInterval(function() {
-    if (connected > 0 && open) {
-        const dataPoint = new DataModel({
-            temperature: Math.floor(Math.random() * 10 + 70),
-            count: Math.floor(Math.random() * 2000) + 5500, 
-            humidity: Math.floor(Math.random() * 5) + 30,
-            pressure: Math.floor(Math.random() * 3) + 29
-        });
-
-        io.emit('update', dataPoint);
-
-        dataPoint.save(function(err) {
-            if (err) {
-                console.log(err);
-            }
-        });
-    }
-}, 1000);
-
-let connected = 0,
-    open = false;
 db.once('open', function() { open = true; });
+
+const io = require('socket.io')(server);
 io.on('connection', function(socket) {
     connected += 1;
     socket.on('disconnect', function() {
@@ -59,7 +43,23 @@ io.on('connection', function(socket) {
     });
 });
 
+const pulse = setInterval(function() {
+    const dataPoint = new DataModel({
+        temperature: Math.floor(Math.random() * 10 + 70),
+        count: Math.floor(Math.random() * 2000) + 5500, 
+        humidity: Math.floor(Math.random() * 5) + 30,
+        pressure: Math.floor(Math.random() * 3) + 29
+    });
 
-// --------------------TODO ------- -FIX SERVER, CHECK STACKOVERFLOW ADVICE 
+    if (connected > 0 && open) {
+        io.emit('update', dataPoint);
+    }
 
-// DO MONGOOSE
+    dataPoint.save(function(err) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log("Saved successfully!");
+        }
+    });
+}, duration);
