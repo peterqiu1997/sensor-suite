a451ac8726fd529ffe0492a1bbcfabdd45d03000
Peter Qiu <peterqiu1997@gmail.com>
2016-07-27 11:04:00 -0700

    refresh button

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 cfg = {};
 cfg.port = process.env.PORT || 3000;
-cfg.uristring = process.env.MONGODB_URI;  // remove this later
+cfg.uristring = process.env.MONGODB_URI || "***REMOVED***";  // remove this later
 
 module.exports = cfg;
--- server.js
+++ server.js
@@ -6,9 +6,7 @@ const compression = require('compression');
 const DataModel = require('./dist/models/DataModel');
 const cfg = require('./config');
 const utils = require('./utils');
-
 const app = express();
-const db = mongoose.connection;
 const duration = 1000;
 let connected = 0,
     statsCounter = 0,
@@ -35,9 +33,15 @@ const io = require('socket.io')(server);
 
 io.on('connection', function(socket) {
     connected += 1;
-    socket.on('request', function() {                
-        console.log("Client requested something. Server side called.");
-        socket.emit('response', "Server gave back this. Connection is open.");
+    socket.on('request', function() {   
+        DataModel.find({ createdAt: utils.past(86400000)}) // data from past day
+             .exec(function(err, result) {
+                if (!err) {
+                    socket.emit('stats', result);
+                } else {
+                    socket.emit('stats', "Error")
+                }
+            });
     });
     socket.on('disconnect', function() {
         connected -= 1;
@@ -45,31 +49,11 @@ io.on('connection', function(socket) {
 });
 
 const pulse = setInterval(function() {
-
-    const dataPoint = new DataModel({
-        temperature: Math.floor(Math.random() * 10 + 70),
-        count: Math.floor(Math.random() * 2000) + 5500, 
-        humidity: Math.floor(Math.random() * 5) + 30,
-        pressure: Math.floor(Math.random() * 3) + 29
-    });
-
-    statsCounter = (statsCounter + 1) % 5;
     if (connected > 0 && open) {
-        io.emit('update', dataPoint);
-        if (statsCounter === 0) {
-            DataModel.find({ createdAt: utils.fetchRange(86400000)})
-             .exec(function(err, result) {
-                if (!err) {
-                    io.emit('stats', result.length);
-                } else {
-                    io.emit('stats', "Error")
-                }
-            });
-        }
+        DataModel.findOne().sort({createdAt: -1}).exec(function(err, result) {
+            if (!err) {
+                io.emit('update', result);
+            }
+        });
     }
-    dataPoint.save(function(err) {
-        if (err) {
-            console.log(err);
-        }
-    });
 }, duration);
