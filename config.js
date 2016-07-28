fe552dd35c916864c299b1aa6d4bf45312752b42
Peter Qiu <peterqiu1997@gmail.com>
2016-07-28 09:55:08 -0700

    heroku local

--- config.js
+++ config.js
@@ -1,7 +1,9 @@
 cfg = {};
 cfg.port = process.env.PORT || 3000;
-cfg.uristring = process.env.MONGODB_URI || "***REMOVED***";  // remove this later
-cfg.ACCOUNT_SID = process.env.ACCOUNT_SID || '***REMOVED***';
-cfg.AUTH_TOKEN = process.env.AUTH_TOKEN || '***REMOVED***';
+cfg.uristring = process.env.MONGODB_URI;
+cfg.ACCOUNT_SID = process.env.TWILIO_ACCOUNT_SID;
+cfg.AUTH_TOKEN = process.env.TWILIO_AUTH_TOKEN;
+cfg.TO_NUMBER = process.env.TO_NUMBER;
+cfg.FROM_NUMBER = process.env.FROM_NUMBER;
 
 module.exports = cfg;
