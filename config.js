f2f2e5728c01f6a329a41d208e01979002a0119a
Peter Qiu <peterqiu1997@gmail.com>
2016-07-29 11:15:04 -0700

    moved twilio

--- config.js
+++ config.js
@@ -1,8 +1,5 @@
 cfg = {};
 cfg.port = process.env.PORT || 3000;
-cfg.uristring = process.env.MONGODB_URI;
-cfg.ACCOUNT_SID = process.env.TWILIO_ACCOUNT_SID;
-cfg.AUTH_TOKEN = process.env.TWILIO_AUTH_TOKEN;
-cfg.FROM_NUMBER = process.env.FROM_NUMBER;
+cfg.uristring = process.env.MONGODB_URI || '***REMOVED***';
 
 module.exports = cfg;
