bbfb7190e34eac3a7057f956a4ac9891f26c89d9
Peter Qiu <peterqiu1997@gmail.com>
2016-07-29 16:01:10 -0700

    demo 29

--- src/components/Statistics.js
+++ src/components/Statistics.js
@@ -13,13 +13,12 @@ export default class Statistics extends React.Component {
     render() { 
         return ( // Text is rendered as span in React 
             <div class = "bottom">
-            <img src = "./img/reload.svg" class = "refresh" onClick = {this.handleClick}/>
-            <div class = "statsTitle">{"Past Day"}</div>
-            <ul class = "statsList">
-                    <li>{"Mean: " + this.props.mean}</li>
-                    <li>{"StdDev: " + this.props.deviation}</li>
-                    <li>{"r: +0.17"}</li>
-                </ul>
+                <div class = "statsTitle">{"Statistics"}</div>
+                    <ul class = "options">
+                            <li>{"Mean: " + this.props.mean}</li>
+                            <li>{"StdDev: " + this.props.deviation}</li>
+                            <li>{"r: +0.17"}</li>
+                    </ul>
             </div>
         );
     }
