8769fd2059dfacb25a9730b7a5f8046bae59e3fb
Peter Qiu <peterqiu1997@gmail.com>
2016-08-05 10:48:17 -0700

    new transporter

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ cfg = {};
 cfg.port = process.env.PORT || 3000;
 cfg.uristring = process.env.MONGODB_URI;
 cfg.EMAILS = 
-cfg.smptConfig = {
+cfg.smtpConfig = {
     service: 'gmail',
     secure: true,
     auth: {
